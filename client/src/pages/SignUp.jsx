0104067898b203b173665959dbd82591d8e7abca
vishnu130902 <vishnu1309n@gmail.com>
2024-06-22 16:52:34 +0530

    added oauth

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
 import {Link, useNavigate} from 'react-router-dom'
+import Oauth from '../components/OAuth'
 
 const SignUp = () => {
   const [formData, setFormData] = useState({})
@@ -83,6 +84,7 @@ const SignUp = () => {
                 'Sign Up'
               )}
             </Button>
+            <Oauth/>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an Account?</span>
