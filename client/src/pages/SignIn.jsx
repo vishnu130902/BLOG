c9e1f74d63fa9a9657ab8a0b71bc2ecfc9ab7053
vishnu130902 <vishnu1309n@gmail.com>
2024-06-22 15:34:43 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,76 +1,89 @@
-import React, { useState } from 'react'
-import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
-import {Link, useNavigate} from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
-const SignIn = () => {
-  const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate()
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value.trim()
-    })
-    console.log(formData)
-  }
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true)
-      setErrorMessage(null)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
       });
-      const data = await res.json()
-      if(data.success===false){
-        setErrorMessage(data.message)
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false)
-      if(res.ok){
-        navigate('/')
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false)
+      dispatch(signInFailure(error.message));
     }
-  }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
               Vishnu's
             </span>
             Blog
-          </Link> 
-            <p className='text-sm mt-5'>
-              You can signip using email and password or google account
-            </p>
+          </Link>
+          <p className='text-sm mt-5'>You can sign in with your email and password
+            or with Google.
+          </p>
         </div>
-
-
+        {/* right */}
 
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Your Email' />
-              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}/>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                onChange={handleChange}
+              />
             </div>
             <div>
-              <Label value='Your Password' />
-              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='**********'
+                id='password'
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
-            {loading ? (
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
                 <>
                   <Spinner size='sm' />
                   <span className='pl-3'>Loading...</span>
@@ -81,7 +94,7 @@ const SignIn = () => {
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Don't have an Account?</span>
+            <span>Dont Have an account?</span>
             <Link to='/sign-up' className='text-blue-500'>
               Sign Up
             </Link>
@@ -94,7 +107,5 @@ const SignIn = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-export default SignIn
