bf386dce1a8872d7a1ee8d40f349e7d8d6a568e8
vishnu130902 <vishnu1309n@gmail.com>
2024-07-16 00:05:39 +0530

    fixed some errors

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({ message: 'API is working!!!'});
 }
@@ -49,3 +53,17 @@ export const updateUser = async (req, res, next) => {
         next(error);
       }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+
