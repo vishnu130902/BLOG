e33c4e36eeac25b74cbdd5ad83189fbb718b4b1b
vishnu130902 <vishnu1309n@gmail.com>
2024-07-18 23:36:21 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -155,6 +155,8 @@ export default function DashProfile() {
       console.log(error.message);
     }
   };
+
+  
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
