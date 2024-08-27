5146a406c1684fac0dcc731e3c2236ae8786e077
vishnu130902 <vishnu1309n@gmail.com>
2024-08-28 00:18:29 +0530

    added recent article section to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -92,7 +92,7 @@ export default function PostPage() {
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
-        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+        <div className='flex flex-row gap-5 mt-5 justify-center flex-wrap lg:flex-nowrap'>
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
